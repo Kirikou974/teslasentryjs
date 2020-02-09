db9099266b4519665c237302fb4795add1c3a906
thierrybigot <thierrybigot@one-share.fr>
2020-02-09 03:05:37 +0100

    added slider to browse video

--- TODO.md
+++ TODO.md
@@ -2,27 +2,26 @@
 
 ## In progress
 
-- Slider pour naviguer dans la vidéo
+- Fix navigation
 
 ## TODO
 
-1. Bouton pour Accélerer video
+1. Bouton d'import
 2. Bouton de suppression de vidéos
-3. Fix navigation
-4. Mise en place du bouton d'import pour fonctionner avec <https://github.com/marcone/teslausb>
-5. Bouton d'import
-6. Process videos before (process on import)
-7. Write installation manual
-8. Improve ffmpeg sync
+3. Mise en place du bouton d'import pour fonctionner avec <https://github.com/marcone/teslausb>
+4. Process videos before (process on import)
+5. Improve ffmpeg sync
+6. Write installation manual
+7. Bouton pour Accélerer video
+8. Implémentation d'OpenCV pour analyze de la vidéo
 9. Bouton de Download video
 
    - Download de vidéo d'un side
    - Download de la vidéo complète
 
-10. Implémentation d'OpenCV pour analyze de la vidéo
-
 ## Done
 
+- Slider pour naviguer dans la vidéo
 - Website
 - PoC
 - Navigation
--- public/js/video.js
+++ public/js/video.js
@@ -1,20 +1,27 @@
-const to = new TIMINGSRC.TimingObject({
-	provider: null,
-	range: [0, 100]
-});
-const skipValue = 15;
+const skipValue = 10;
+const progressSlider = '#progressSlider';
 
-to.on('timeupdate', function() {
-	let currentPosition = getVideoPosition();
-	// $('#progress').val(currentPosition);
-});
+function videoPaused() {
+	$('#pause').hide();
+	$('#play').show();
+}
+function videoPlaying() {
+	setPlayUI();
+}
+function videoPlay() {
+	setPlayUI();
+}
+function setPlayUI() {
+	$('#pause').show();
+	$('#play').hide();
+}
 
-function syncVideos(action) {
-	// set up video sync
-	MCorp.mediaSync(document.getElementById('videofront'), to);
-	MCorp.mediaSync(document.getElementById('videoback'), to);
-	MCorp.mediaSync(document.getElementById('videoleftrepeater'), to);
-	MCorp.mediaSync(document.getElementById('videorightrepeater'), to);
+function disableSlider() {
+	$(progressSlider).off('input');
+}
+
+function enableSlider() {
+	$(progressSlider).on('input', changeVideoPosition);
 }
 
 // Later on, after some condition has been met, set video source to the
@@ -28,43 +35,50 @@ function loadVideos(baseVideoUrl) {
 	videoleftrepeater.poster = `${baseVideoUrl}/left_repeater/poster.jpg`;
 	videorightrepeater.src = `${baseVideoUrl}/right_repeater`;
 	videorightrepeater.poster = `${baseVideoUrl}/right_repeater/poster.jpg`;
-
-	videofront.addEventListener('durationchange', loadSeekbar, false);
-
-	syncVideos();
 }
 
-function loadSeekbar() {
-	// to.range = [0, videofront.duration];
-	// progress.max = videofront.duration;
+function getVideoDuration() {
+	let videoDuration = videofront.duration;
+	return videoDuration;
+}
+
+function syncVideos() {
+	let videoDuration = getVideoDuration();
+	$('#lblTotalTime').text(timeFormat(videoDuration));
+	$(progressSlider).attr('max', videoDuration);
+
+	to = new TIMINGSRC.TimingObject({
+		provider: null,
+		range: [0, videoDuration]
+	});
+	MCorp.mediaSync(document.getElementById('videofront'), to);
+	MCorp.mediaSync(document.getElementById('videoback'), to);
+	MCorp.mediaSync(document.getElementById('videoleftrepeater'), to);
+	MCorp.mediaSync(document.getElementById('videorightrepeater'), to);
+
+	to.on('timeupdate', function() {
+		disableSlider();
+		$(progressSlider).val(to.query().position.toFixed(2));
+		updateSliderColor();
+
+		$('#lblElapsedTime').text(timeFormat(videofront.currentTime));
+		enableSlider();
+	});
 }
 
 function changeVideoPosition() {
-	let currentVelocity = getVideoVelocity();
-
-	setVideoVelocity(0.0);
-	setVideoPosition(parseFloat(progress.value).toFixed(1));
-	setTimeout(() => {
-		setVideoVelocity(currentVelocity);
-	}, 1000);
+	setVideoPosition(parseFloat($(progressSlider).val()));
 }
 
-function togglePlayPause() {
-	$('#pause').toggle(0);
-	$('#play').toggle(0);
-}
 function playVideo() {
-	togglePlayPause();
 	setVideoVelocity(1.0);
 }
 
 function pauseVideo() {
-	togglePlayPause();
 	setVideoVelocity(0.0);
 }
-
 function accelerateVideo() {
-	//TODO
+	//TODO: performance issues
 }
 function skipVideoForward() {
 	skipVideo(skipValue);
@@ -95,5 +109,66 @@ function setVideoVelocity(velocity) {
 }
 
 function stopVideo() {
-	setVideoPosition(100.0);
+	setVideoPosition(0.0);
+	if (!videofront.paused) {
+		pauseVideo();
+	}
+}
+
+function updateSliderColor() {
+	//Slider coloration : https://stackoverflow.com/questions/27153035/styling-the-html5-range-bar-left-of-thumb-right-of-thumb
+	let progressSliderObject = $(progressSlider)[0];
+	let currentValue = progressSliderObject.value;
+	let videoDuration = getVideoDuration();
+	let v = (currentValue / videoDuration + 0.01) * 100;
+
+	progressSliderObject.style.background =
+		'-moz-linear-gradient(left,  #ed1e24 0%, #ed1e24 ' +
+		v +
+		'%, #d3d3d3 ' +
+		v +
+		'%, #d3d3d3 100%)';
+	progressSliderObject.style.background =
+		'-webkit-gradient(linear, left top, right top, color-stop(0%,#ed1e24), color-stop(' +
+		v +
+		'%,#ed1e24), color-stop(' +
+		v +
+		'%,#d3d3d3), color-stop(100%,#d3d3d3))';
+	progressSliderObject.style.background =
+		'-webkit-linear-gradient(left,  #ed1e24 0%,#ed1e24 ' +
+		v +
+		'%,#d3d3d3 ' +
+		v +
+		'%,#d3d3d3 100%)';
+	progressSliderObject.style.background =
+		'-o-linear-gradient(left,  #ed1e24 0%,#ed1e24 ' +
+		v +
+		'%,#d3d3d3 ' +
+		v +
+		'%,#d3d3d3 100%)';
+	progressSliderObject.style.background =
+		'-ms-linear-gradient(left,  #ed1e24 0%,#ed1e24 ' +
+		v +
+		'%,#d3d3d3 ' +
+		v +
+		'%,#d3d3d3 100%)';
+	progressSliderObject.style.background =
+		'linear-gradient(to right,  #ed1e24 0%,#ed1e24 ' +
+		v +
+		'%,#d3d3d3 ' +
+		v +
+		'%,#d3d3d3 100%)';
+}
+
+//https://maqentaer.com/devopera-static-backup/http/dev.opera.com/articles/view/custom-html5-video-player-with-css3-and-jquery/index.html
+function timeFormat(seconds) {
+	var m =
+		Math.floor(seconds / 60) < 10
+			? '0' + Math.floor(seconds / 60)
+			: Math.floor(seconds / 60);
+	var s =
+		Math.floor(seconds - m * 60) < 10
+			? '0' + Math.floor(seconds - m * 60)
+			: Math.floor(seconds - m * 60);
+	return m + ':' + s;
 }
